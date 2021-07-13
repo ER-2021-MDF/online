bb3f261b590f54c09cc879965a6a482e1e3c5b62
ER-2021-MDF <85852542+ER-2021-MDF@users.noreply.github.com>
2021-07-13 16:21:19 +0200

    Add files via upload

--- includiSwipe.js
+++ includiSwipe.js
@@ -21,3 +21,107 @@ hammertimeC.on('swipeleft', function(ev) {
 hammertimeC.on('swiperight', function(ev) {
 	plusSlides(1, 2);
 });
+
+var hammertimeD = new Hammer(document.getElementById('DWrapper'));
+hammertimeD.on('swipeleft', function(ev) {
+	plusSlides(-1, 3);
+}); 
+hammertimeD.on('swiperight', function(ev) {
+	plusSlides(1, 3);
+});
+
+var hammertimeE = new Hammer(document.getElementById('EWrapper'));
+hammertimeE.on('swipeleft', function(ev) {
+	plusSlides(-1, 4);
+}); 
+hammertimeE.on('swiperight', function(ev) {
+	plusSlides(1, 4);
+});
+
+var hammertimeF = new Hammer(document.getElementById('FWrapper'));
+hammertimeF.on('swipeleft', function(ev) {
+	plusSlides(-1, 5);
+}); 
+hammertimeF.on('swiperight', function(ev) {
+	plusSlides(1, 5);
+});
+
+var hammertimeG = new Hammer(document.getElementById('GWrapper'));
+hammertimeG.on('swipeleft', function(ev) {
+	plusSlides(-1, 6);
+}); 
+hammertimeG.on('swiperight', function(ev) {
+	plusSlides(1, 6);
+});
+
+var hammertimeH = new Hammer(document.getElementById('HWrapper'));
+hammertimeH.on('swipeleft', function(ev) {
+	plusSlides(-1, 7);
+}); 
+hammertimeH.on('swiperight', function(ev) {
+	plusSlides(1, 7);
+});
+
+var hammertimeJ = new Hammer(document.getElementById('JWrapper'));
+hammertimeJ.on('swipeleft', function(ev) {
+	plusSlides(-1, 8);
+}); 
+hammertimeJ.on('swiperight', function(ev) {
+	plusSlides(1, 8);
+});
+
+var hammertimeK = new Hammer(document.getElementById('KWrapper'));
+hammertimeK.on('swipeleft', function(ev) {
+	plusSlides(-1, 9);
+}); 
+hammertimeK.on('swiperight', function(ev) {
+	plusSlides(1, 9);
+});
+
+var hammertime = new Hammer(document.getElementById('LWrapper'));
+hammertimeL.on('swipeleft', function(ev) {
+	plusSlides(-1, 10);
+}); 
+hammertimeL.on('swiperight', function(ev) {
+	plusSlides(1, 10);
+});
+
+var hammertime = new Hammer(document.getElementById('MWrapper'));
+hammertimeM.on('swipeleft', function(ev) {
+	plusSlides(-1, 11);
+}); 
+hammertimeM.on('swiperight', function(ev) {
+	plusSlides(1, 11);
+});
+
+var hammertime = new Hammer(document.getElementById('NWrapper'));
+hammertimeN.on('swipeleft', function(ev) {
+	plusSlides(-1, 12);
+}); 
+hammertimeN.on('swiperight', function(ev) {
+	plusSlides(1, 12);
+});
+
+var hammertime = new Hammer(document.getElementById('OWrapper'));
+hammertimeO.on('swipeleft', function(ev) {
+	plusSlides(-1, 13);
+}); 
+hammertimeO.on('swiperight', function(ev) {
+	plusSlides(1, 13);
+});
+
+var hammertime = new Hammer(document.getElementById('PWrapper'));
+hammertimeP.on('swipeleft', function(ev) {
+	plusSlides(-1, 14);
+}); 
+hammertimeP.on('swiperight', function(ev) {
+	plusSlides(1, 14);
+});
+
+var hammertime = new Hammer(document.getElementById('QWrapper'));
+hammertimeQ.on('swipeleft', function(ev) {
+	plusSlides(-1, 15);
+}); 
+hammertimeQ.on('swiperight', function(ev) {
+	plusSlides(1, 15);
+});
